309652593a2c3b7c54a6912c3a685326b00f4426
Jason Taylor <jason@screencasts.pro>
2014-08-13 00:21:56 -0400

    Adding main recipe for new cookbook

---
+++ chef/cookbooks/vagrant_tomcat/recipes/default.rb
@@ -0,0 +1,21 @@
+include_recipe "tomcat_latest"
+
+file "/etc/rc.d/tomcat" do
+  action :delete
+end
+
+template "/etc/init.d/tomcat7" do
+  source "tomcat7.erb"
+  mode "0755"
+  owner "root"
+  group "root"
+  notifies :restart, "service[tomcat7]"
+end
+
+template "#{node[:tomcat_latest][:tomcat_instal_loc]}/tomcat/apache-tomcat/conf/tomcat-users.xml"
+
+service "tomcat7" do
+  service_name "tomcat7"
+  supports :restart => true, :status => true
+  action [:start, :enable]
+end
