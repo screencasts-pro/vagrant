deb6217746e1372cf43c45b55fdcab60d5c5505d
Jason Taylor <jason@screencasts.pro>
2014-08-13 00:06:48 -0400

    adding overriding attributes for new cookbook

---
+++ chef/cookbooks/vagrant_tomcat/attributes/default.rb
@@ -0,0 +1,5 @@
+default["tomcat_latest"]["tomcat_version"] = "7"
+default["tomcat_latest"]["tomcat_install_loc"] = "/usr/local"
+default["tomcat_latest"]["direct_download_version"] = "7.0.55"
+default["tomcat_latest"]["tomcat_user"] = "root"
+default["vagrant_tomcat"]["tomcat_home"] = "#{node["tomcat_latest"]["tomcat_install_loc"]}/tomcat/apache-tomcat"
